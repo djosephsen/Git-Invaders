837a33e442c2fb9befae1ef16d37df60e25ccb14
Dave Josephsen <dave@skeptech.org>
2014-02-05 23:27:41 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 Git-Invaders
 ============
 
-Git invaders is the collection of software I will use to maintain a space-invaders github graph that consists of meaningful commits (no cheating)
+Git invaders is the collection of software I will use to maintain a
+space-invaders github graph. 
+
+## hax!
+
+Yeah, I know, my github graph is the sacred record of my productivity, and I
+shouldn't mess around with it or people won't take me seriously. I'm sorry, but
+having had this idea, I can't not do it, so here's the deal: 
+
+I'm not going to generate false commits to make this work, all the work you see
+on the graph was real work done by me and not some shell script drawing pretty
+pictures by adding spaces to a text file. 
+
+## How?
+
+tldr; by automatically delaying some of my commits by redirecting them to a
+private github server.
+
+There are three pieces to this puzzle. The first is a Go program that models
+the 'invaders' data structure, and given epoc values for the start day and
+today, returns 0 if it's ok to git push today, and 1 if it's not ok to git push
+today. 
+
+The second is a shell wrapper for git that intercepts my push commands and, if
+I shouldn't push to github today, merges the github repo with a private copy
+and then pushes to the private copy instead. 
+
+The third is a cron script that runs against the private staging server and
+attempts to dump any changes I've staged. It's not in the spirit of this to
+"save up" my commits and spread them or anything like that; I'm just delaying
+my commits on certain days of the week, and if I don't have the commit traffic
+to pull this off, then there will be holes in the graph. I'll live with it. I'm
+just a developer evangalist, so I don't expect a day of delay here and there to
+kill anyone. If it gets in the way I'll stop doing it. 
---
+++ invaders.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+   "os"
+	"time"
+	"strconv"
+)
+
+//globals
+var invaders = [][]bool{ 
+	{false,false,false,true,true,true,false,}, 
+	{false,false,true,true,false,false,false,}, 
+	{false,true,true,true,true,true,false,}, 
+	{true,true,true,false,true,false,false,}, 
+	{false,true,true,true,true,false,false,}, 
+	{false,true,true,true,true,false,false,}, 
+	{false,true,true,true,true,false,false,}, 
+	{true,true,true,false,true,false,false,}, 
+	{false,true,true,true,true,true,false,}, 
+	{false,false,true,true,false,false,false,}, 
+	{false,false,false,true,true,true,false,},
+	{false,false,true,true,true,false,false,}, 
+	{false,false,true,true,true,false,true,}, 
+	{false,true,true,true,true,true,false,}, 
+	{true,true,true,false,true,false,false,}, 
+	{true,true,true,true,true,true,false,}, 
+	{true,true,true,true,true,true,false,}, 
+	{true,true,true,false,true,false,false,}, 
+	{false,true,true,true,true,true,false,}, 
+	{false,false,true,true,true,false,true,},
+	{false,false,true,true,true,false,false,},
+	{false,false,true,true,true,false,false,}, 
+	{false,false,true,true,false,false,true,}, 
+	{false,true,true,true,false,true,false,}, 
+	{false,true,false,true,true,false,true,}, 
+	{true,true,true,true,false,true,false,}, 
+	{true,true,true,true,false,true,false,}, 
+	{false,true,false,true,true,false,true,}, 
+	{false,true,true,true,false,true,false,}, 
+	{false,false,true,true,false,false,true,},
+	{false,false,true,true,true,false,false,}, }
+
+func testInvader(invader [][]bool, wo int, do int){
+//print the invader patterns to the console
+
+	for d:=0; d<=6; d++ {
+		for w:=0; w<=30; w++ {
+			if d==do && w==wo {
+				fmt.Print("T")
+			}else if invaders[w][d] {
+				fmt.Print("#")
+			}else{
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+
+var start, today, weekOffset, dayOffset int
+
+
+// Deal with input
+if len(os.Args) > 1 {
+	start,_=strconv.Atoi(os.Args[1])
+}else{
+	fmt.Println("error: need start time in epoc seconds for arg1")
+	os.Exit(42)
+}
+
+if len(os.Args) > 2 {
+	today,_=strconv.Atoi(os.Args[2])
+}else{
+	t:=time.Now()
+	today=int(t.Unix())
+}
+startTime:=time.Unix(int64(start),0)
+todayTime:=time.Unix(int64(today),0)
+
+if todayTime.Before(startTime){
+	fmt.Println("Error: Start time must precede End time")
+	os.Exit(42)
+}
+
+// compute some interesting lengths
+daysPerWeek := 7
+daysTot := len(invaders)*len(invaders[0])
+daysSinceStart := (todayTime.Sub(startTime)/(time.Hour*24))
+
+if (int(daysSinceStart) <= daysTot){
+	weekOffset = (int(daysSinceStart) / daysPerWeek)
+	dayOffset = (int(daysSinceStart) % daysPerWeek)
+}else{
+	effectiveDaysSinceStart := (int(daysSinceStart) % daysTot)
+	weekOffset = effectiveDaysSinceStart / daysPerWeek
+	dayOffset = effectiveDaysSinceStart % daysPerWeek
+}
+
+fmt.Printf("start: %d, today %d, daysSinceStart: %d\n",start, today, daysSinceStart)
+fmt.Printf("daystot: %d\n", daysTot)
+
+testInvader(invaders, weekOffset, dayOffset )
+
+if invaders[weekOffset][dayOffset] { 
+	fmt.Println("returning true")
+}else{
+	fmt.Println("returning false")
+}
+
+
+}
