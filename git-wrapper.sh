28555dfba89f8711b41bf4535ac285b9f9419f54
Dave Josephsen <dave@skeptech.org>
2014-02-06 19:36:57 -0600

    bugz

--- git-wrapper.sh
+++ git-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PATTERN_REPEAT=216 #a magic number, where the invader pattern first repeats. 
-START_DAY='1391983086'
+START_DAY='1391925600'
 RG=/usr/bin/git
 IV=${HOME}/reps/go/bin/invaders
 DFTL=/usr/local/Cellar/libfaketime/0.9.5/lib/faketime/libfaketime.1.dylib
@@ -39,25 +39,26 @@ if grep -q 'commit' <<< $@
 then
 	if ! ${IV} ${START_DAY} > /dev/null
 	then
-		DSS=$((($(date +%s)-${START_DAY})/86400))
-		i=$((${PATTERN_REPEAT} + ${DSS} ))
+		DSS=$((($(date +%s)-${START_DAY})/86400)) #days since start
+		i=$((${PATTERN_REPEAT} + ${DSS} - 1 ))
+		c=0 # keep an extra variable for the day offset; things are confusing enough as is
 		debug "start: $DSS days since start, I is $i (${PATTERN_REPEAT} + ${DSS} )"
-		while ! ${IV} ${START_DAY} $(computeEpoc ${i}) > /dev/null
+		while ! ${IV} ${START_DAY} $(computeEpoc ${i}) 
 		do
 			debug "loop: I is $i"
 			i=$((${i}-1))
+			c=$((${c}+1))
 		done
-		debug "done: I is $i"
-		OFFSET=$((${PATTERN_REPEAT}+${DSS}-${i}))
-
+		debug "done: I is $i C is $c offset is $c (${PATTERN_REPEAT}-${i})"
+ 
 		if [ "${ARCH}" == Darwin ]
 		then
-			DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES=${DFTL} FAKETIME="-${OFFSET}d" ${RG} "${@}"
-			debug " Invaders Gitwrapper Execing: DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES=${DFTL} FAKETIME=\"-${OFFSET}d\" ${RG} ${@}"
+			DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES=${DFTL} FAKETIME="-${c}d" ${RG} "${@}"
+			debug " Invaders Gitwrapper Execing: DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES=${DFTL} FAKETIME=\"-${c}d\" ${RG} ${@}"
 		elif [ "${ARCH}" == Linux ]
 		then
-			LD_PRELOAD=${LFTL} FAKETIME="-${OFFSET}d" ${RG} "${@}"
-			debug "Invaders Gitwrapper Execing: LD_PRELOAD=${LFTL} FAKETIME=\"-${OFFSET}d\" ${RG} ${@}"
+			LD_PRELOAD=${LFTL} FAKETIME="-${c}d" ${RG} "${@}"
+			debug "Invaders Gitwrapper Execing: LD_PRELOAD=${LFTL} FAKETIME=\"-${c}d\" ${RG} ${@}"
 		else
 			error "ARCH not detected (neither Linux nor Darwin)"
 		fi
